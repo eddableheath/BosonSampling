5c6ca3d76f9fc1c074f3142669bad5ae3aa00956
Edmund Dable-Heath <eddableheath@gmail.com>
2019-10-29 14:29:58 +0000

    - Continued to implement listicle things

--- LaplaceExpansion.py
+++ LaplaceExpansion.py
@@ -12,14 +12,12 @@ def PartialSum(B_k, k, delta):
     :param B_k: B_k matrix - unitary missing k'th column
     :param k: sequence marker
     :param delta: delta from {-1,1}^{k-1} space
-    :return: dictionary {j: v_j(delta)} where v_j(delta) is partial sum
+    :return: list of partial sums
     """
-    sums = []
-    for j in range(0, k):
-        sum = 0
-        for i in range(0, k-1):
-            sum += B_k[i][j] * delta[i]
-        sums.append(sum)
+    sums = [
+        sum([B_k[i][j] * delta[i] for i in range(0, k-1)])
+            for j in range(0, k)
+    ]
     return sums
 
 
@@ -28,16 +26,16 @@ def ForwardPartialProd(PartSums, k):
     """
     :param PartSums: dictionary of partial sums
     :param k: sequence marker
-    :return: dictionary {l: f_l(delta)} where f_l is forward partial product, f_1 = 1
+    :return: list of forward partial products
     """
-    FProds = {1: 1}
+    FProds = [1]
     for l in range(1, k):
         prod = 1+0j
         for j in range(0, l):
             print(j)
             prod *= PartSums[j]
             print('fprod:', prod)
-        FProds[l+1] = prod
+        FProds.append(prod)
     return FProds
 
 
@@ -48,16 +46,13 @@ def BackwardPartialProd(PartSums, k):
     :param k: sequence marker
     :return: dictionary {l: b_l(delta)} where b_l is backward partial product, b_k = 1
     """
-    BProds = {}
-    for l in range(0, k-1):
-        prod = 1+0j
-        for j in range(l, k -1):
-            print(j)
-            prod *= PartSums[j]
-            print('bprod:', prod)
-        BProds[l+1] = prod
-    BProds[k] = 1
+    BProds = [
+        np.prod([PartSums[j] for j in range (l, k-1)])
+        for l in range(0, k-1)
+    ]
+    BProds.append(1)
     return BProds
+
 
 # Partial products:
 def PartialProducts(B_k, k):
@@ -83,6 +78,10 @@ def PartialProducts(B_k, k):
     return PartProds
 
 
+def SignAlt(n):
+    return (-1+0j)**n
+
+
 def LaplaceExpansion(B_k, k):
     """
     :param B_k: Reduced unitary matrix for permanent
@@ -90,13 +89,11 @@ def LaplaceExpansion(B_k, k):
     :return: sequence of permanents of reduced matrices
     """
     PartProds = PartialProducts(B_k, k)
-    Perms = []
-    for l in range(0, k):
-        sum = 0
-        count = 0
-        for i in PartProds:
-            sum += ((-1+0j)**count) * i[l]
-            count += 1
-        sum *= (1/(2**(k-2))) + 0j
-        Perms.append(sum)
+    Perms = [
+        (1 / (2 ** (k - 2))) + 0j * sum([
+            SignAlt(n)*PartProds[n][l]
+            for n in range(0,2**(k-1))
+        ])
+        for l in range(0,k)
+    ]
     return Perms
