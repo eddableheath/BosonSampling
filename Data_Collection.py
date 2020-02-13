2b4c590f31673d992b8f6b1d048e7fadcba5bb58
Edmund Dable-Heath <eddableheath@gmail.com>
2020-02-13 17:32:51 +0000

    - more changes to data collection

--- Data_Collection.py
+++ Data_Collection.py
@@ -14,20 +14,32 @@ lengths = [np.linalg.norm(sum([ls.sampler(4, np.genfromtxt('Lattices/4/0/0.csv',
 if 0.0 in lengths:
     lengths.remove(0.0)
 
-#adj_lengths = lengths/shortest_vec THIS DOESN'T WORK LIKE THIS
+adj_lengths = [x / shortest_vec for x in lengths]
 
 
 #distances = [np.linalg.norm(sum([ls.sampler(4, np.genfromtxt('Lattices/4/0/0.csv', delimiter=',', dtype=None),
 #                                            np.genfromtxt('Unitaries/8/0.csv', delimiter=',', dtype=None))
 #                                for i in range(8)])/shortest_vec) for j in range(10000)]
 
+print('regular lengths ------------')
 print('minimum', min(lengths))
 print('maximum', max(lengths))
 print('mean', st.mean(lengths))
 print('mode', st.mode(lengths))
 print('median', st.median(lengths))
+print('ratio lengths --------------')
+print('minimum', min(adj_lengths))
+print('maximum', max(adj_lengths))
+print('mean', st.mean(adj_lengths))
+print('mode', st.mode(adj_lengths))
+print('median', st.median(adj_lengths))
 print('shortest vector', np.genfromtxt('Lattices/4/0/5.csv', delimiter=',', dtype=None))
+
 fig1, ax1 = plt.subplots()
-ax1.set_title('Test plot')
+ax1.set_title('Lengths')
 ax1.boxplot(lengths)
+
+fig2, ax2 = plt.subplots()
+ax2.set_title('Ratio')
+ax2.boxplot(adj_lengths)
 plt.show()
